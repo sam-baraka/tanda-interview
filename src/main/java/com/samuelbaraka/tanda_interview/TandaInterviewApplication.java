1ac5d9e1251e25df3730a2b76d10a088c40b8b80
Samuel Baraka <sababuvercetti@gmail.com>
2024-07-17 14:41:14 +0300

    Initial commit

---
+++ src/main/java/com/samuelbaraka/tanda_interview/TandaInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.samuelbaraka.tanda_interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TandaInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TandaInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samuelbaraka/tanda_interview/TandaInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuelbaraka.tanda_interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TandaInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
