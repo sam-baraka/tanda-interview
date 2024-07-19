e777f1244ac5cd9de94b5e020354cf93fc4ae40d
Samuel Baraka <sababuvercetti@gmail.com>
2024-07-19 14:19:47 +0300

    Remove unused imports

--- src/main/java/com/samuelbaraka/tanda_interview/controller/B2CController.java
+++ src/main/java/com/samuelbaraka/tanda_interview/controller/B2CController.java
@@ -7,8 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
-import javax.validation.ConstraintViolationException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 
 
 @RestController
